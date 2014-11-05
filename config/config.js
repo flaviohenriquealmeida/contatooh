69e76a8c2a98c0ded4025336bea8974ad353c066
flaviohenriquealmeida <flaviohenriquealmeida@gmail.com>
2014-11-05 15:50:44 -0200

    single que não era singleton e meta view port faltando auth.ejs

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
-var config;
 module.exports = function() {
-	if(!config)
-		config = require('./env/' + (process.env.NODE_ENV || 'development') + '.js');
-	return config;
+	
+	return require('./env/' + (process.env.NODE_ENV || 'development') + '.js');
 }
