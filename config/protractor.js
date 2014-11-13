9bfe496cdb4f0512563a19c3b8ddfd976eb99adf
flaviohenriquealmeida <flaviohenriquealmeida@gmail.com>
2014-11-13 15:56:19 -0200

    tentando

--- config/protractor.js
+++ config/protractor.js
@@ -13,6 +13,8 @@ exports.config = {
   specs: ['../test/e2e/**/*Spec.js'],
   onPrepare: function() {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().pageLoadTimeout(40000);
+    browser.manage().timeouts().implicitlyWait(25000);
     browser.get('http://localhost:3000');
     browser.driver.findElement(by.id('entrar')).click();
     //element(by.id('entrar')).click(); 
