5ae480c4e713d57c687e622f039574f198595a13
flaviohenriquealmeida <flaviohenriquealmeida@gmail.com>
2014-11-26 11:50:50 -0200

    teste

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		console.log(req.user);
-		res.render('index', { "usuarioLogado" : req.user.nome});
+		
+		//console.log(req.user);
+		//res.render('index', { "usuarioLogado" : req.user.nome});
+		res.render('index', { "usuarioLogado" : 'teste'});
 	});
 };
--- server.js
+++ server.js
@@ -7,17 +7,7 @@ require('./config/express')(app);
 require('./config/passport')();
 require('./config/database')(config.db); 
 
-/*
 http.createServer(app).listen(config.port, config.address, function(){
-  console.log('Express Https Server ' 
-  	+ config.address 
-  	+ ' (' + config.env 
-  	+ ') escutando na porta ' + config.port);
-});
-
-*/
-
-http.createServer(app).listen(config.port, function(){
   console.log('Express Https Server ' 
   	+ config.address 
   	+ ' (' + config.env 
