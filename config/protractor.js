9b89e922e184cfe9d292a0f2b0d36bee4b70bf43
flaviohenriquealmeida <flaviohenriquealmeida@gmail.com>
2014-11-26 13:04:52 -0200

    ultima tentativa

--- config/protractor.js
+++ config/protractor.js
@@ -13,12 +13,16 @@ exports.config = {
 
   specs: ['../test/e2e/**/*Spec.js'],
   onPrepare: function() {
-    browser.ignoreSynchronization = true;
-    browser.get('http://localhost:3000');
-    element(by.id('entrar')).click(); 
-    element(by.id('login_field')).sendKeys(config.seleniumUser);
-    element(by.id('password')).sendKeys(config.seleniumUserPassword);
-    element(by.name('commit')).click();
-    browser.ignoreSynchronization = false;
+    //browser.ignoreSynchronization = true;
+    browser.driver.get('http://localhost:3000');
+    browser.driver.findElement(by.id('entrar')).click()
+    //element(by.id('entrar')).click(); 
+    browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
+    //element(by.id('login_field')).sendKeys(config.seleniumUser);
+    browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
+    //element(by.id('password')).sendKeys(config.seleniumUserPassword);
+     browser.driver.findElement(by.name('commit')).click();
+    //element(by.name('commit')).click();
+    //browser.ignoreSynchronization = false;
   }
 };
