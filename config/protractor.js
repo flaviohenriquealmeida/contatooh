3724f1030cb625769e8c6c937faffd6247ebac46
flaviohenriquealmeida <flaviohenriquealmeida@gmail.com>
2015-05-28 17:51:39 -0300

    Tweaking protractor wait for page load

--- config/protractor.js
+++ config/protractor.js
@@ -13,16 +13,11 @@ exports.config = {
 
   specs: ['../test/e2e/**/*Spec.js'],
   onPrepare: function() {
-    //browser.ignoreSynchronization = true;
-    browser.driver.get('http://localhost:3000');
-    browser.driver.findElement(by.id('entrar')).click()
-    //element(by.id('entrar')).click(); 
-    browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
-    //element(by.id('login_field')).sendKeys(config.seleniumUser);
-    browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
-    //element(by.id('password')).sendKeys(config.seleniumUserPassword);
-     browser.driver.findElement(by.name('commit')).click();
-    //element(by.name('commit')).click();
-    //browser.ignoreSynchronization = false;
+    browser.driver.get('http://localhost:3000').then(function() {
+      browser.driver.findElement(by.id('entrar')).click()
+      browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
+      browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
+      browser.driver.findElement(by.name('commit')).click();
+    });
   }
 };
